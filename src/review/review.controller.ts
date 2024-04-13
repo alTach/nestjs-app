2aa9e86f649d8bb5e895acaab335abf6b70a233e
Алиев Турач Сураддин-оглы <alievts@mosreg.ru>
2024-04-14 00:20:58 +0300

    feat: crete review controller

---
+++ src/review/review.controller.ts
@@ -0,0 +1,20 @@
+import { Body, Controller, Delete, Get, Param, Post } from '@nestjs/common';
+import { ReviewModel } from "src/review/review.model";
+
+@Controller('review')
+export class ReviewController {
+  @Post('create')
+  async create(@Body() dto: Omit<ReviewModel, '_id'>) {
+
+  }
+
+  @Delete(':id')
+  async delete(@Param() id: string) {
+
+  }
+
+  @Get('getByProduct/:productId')
+  async getByProduct(@Param('productId') productId: string ) {
+
+  }
+}
